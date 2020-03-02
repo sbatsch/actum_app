93022d061189f98e5dad17825212a73ea2e7393e
sbatsch <sbatsch2@gmail.com>
2020-03-02 17:03:57 -0600

    Fully working crud app with authentication and validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,12 @@
 class Question < ApplicationRecord
+  validates :description, presence: true 
+  validates :description, uniqueness: true 
+  validates :description, length: { maximum: 500 }
+  validates :category, presence: true
+  validates :difficulty_level, presence: true
+  validates :answer_a, presence: true
+  validates :answer_b, presence: true
+  validates :answer_c, presence: true
+  validates :answer_d, presence: true
+  validates :true_false, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
+  namespace :api do
+  get '/questions' => 'questions#index'
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  post '/questions' => 'questions#create'
+  post "/users" => "users#create"
+  post "/sessions" => "sessions#create"
+  
+  get '/questions/:id' => 'questions#show'
+  patch '/questions/:id' => 'questions#update'
+  delete '/questions/:id'=> 'questions#destroy'  
+  end
 end
