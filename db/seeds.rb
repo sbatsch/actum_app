f72f707977f1b1f79fd38290550e848c480c06e4
sbatsch <sbatsch2@gmail.com>
2020-03-04 10:58:06 -0600

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,309 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
    
-  # Question.create(description:"True or False: When generating a model, the model name is singular.", category: "Rails",difficulty_level:1, answer_key:"true", answer_a:"", answer_b:"", answer_c:"", answer_d:"", true_false:true)
+  # Question.create!(
+                  # description: "True or False: When generating a model, the model name is singular.", 
+                  # category: "Rails",
+                  # difficulty_level: 1, 
+                  # answer_key: "true", 
+                  # answer_a: "", 
+                  # answer_b: "", 
+                  # answer_c: "", 
+                  # answer_d: "", 
+                  # true_false: true
+                  # )
 
-  # Question.create(description:"True or False: When generating a controller, the controller name after 'api/' is singular.", category: "Rails", difficulty_level:1, answer_key:"false", answer_a:"", answer_b:"", answer_c:"", answer_d:"", true_false:false)
+  # Question.create!(
+                  # description: "True or False: When generating a controller, the controller name after 'api/' is singular.", 
+                  # category: "Rails", 
+                  # difficulty_level: 1, 
+                  # answer_key: "false", 
+                  # answer_a: "", 
+                  # answer_b: "", 
+                  # answer_c: "", 
+                  # answer_d: "", 
+                  # true_false: true
+                  # )
+
+# Steve Questions
+  # =========================================================================
+
+# Question.create!(
+#                   description: "How do you add information from the seed file to the database?", 
+#                   category: "Rails", 
+#                   difficulty_level: 1, 
+#                   answer_key: "Rake db:seed", 
+#                   answer_a: "Rails db:seeds", 
+#                   answer_b: "Rake seed", 
+#                   answer_c: "Rake db:seeds", 
+#                   answer_d: "Rails seed data", 
+#                   true_false: false
+#                   )
+
+# Question.create!(
+#                   description: "Name the restful routes?", 
+#                   category: "Rails", 
+#                   difficulty_level: 4, 
+#                   answer_key: "Index, New, Create, Show, Edit, Update, Destroy", 
+#                   answer_a: "Index, New, Make, Show, Edit, Update, Destroy", 
+#                   answer_b: "Index, New, Create, View, Edit, Update, Destroy", 
+#                   answer_c: "Index, New, Post, Show, Edit, Update, Delete", 
+#                   answer_d: "Index, New, Create, Show, Edit, Update, Destroy", 
+#                   true_false: false
+#                   )
+
+# Question.create!(
+#                   description: "What Does the acronym CRUD stand for?", 
+#                   category: "Rails", 
+#                   difficulty_level: 2, 
+#                   answer_key: "Create Read Update Destroy", 
+#                   answer_a: "Create Read Update Destroy", 
+#                   answer_b: "Create Read Update Delete", 
+#                   answer_c: "Create Rails Update Database", 
+#                   answer_d: "Create Render Update Destroy", 
+#                   true_false: false
+#                   )
+
+# Question.create!(
+#                   description: "Which restful routes require a (wild card)id?", 
+#                   category: "Rails", 
+#                   difficulty_level: 3, 
+#                   answer_key: "Show, Update, Delete", 
+#                   answer_a: "Update, Show, Create", 
+#                   answer_b: "Show, Update, Delete", 
+#                   answer_c: "Read, Delete, Show", 
+#                   answer_d: "Index, Show, Update", 
+#                   true_false: false
+#                   )
+
+# Question.create!(
+#                   description: "In Rails, what folder would you find the routes file?", 
+#                   category: "Rails", 
+#                   difficulty_level: 2, 
+#                   answer_key: "config", 
+#                   answer_a: "db", 
+#                   answer_b: "config", 
+#                   answer_c: "public", 
+#                   answer_d: "app", 
+#                   true_false: false
+#                   )
+
+# Question.create!(
+#                   description: "Where would you find this line of code: 'protect_from_forgery with: :null_session'?", 
+#                   category: "Rails", 
+#                   difficulty_level: 4, 
+#                   answer_key: "application_controller.rb", 
+#                   answer_a: "applications_controller.rb", 
+#                   answer_b: "config.rb", 
+#                   answer_c: "application.controller.rb", 
+#                   answer_d: "manifest.js", 
+#                   true_false: false
+#                   )
+
+# Question.create!(
+#                   description: "What does the 'REST' in RESTful routes stand for?", 
+#                   category: "Rails", 
+#                   difficulty_level: 5, 
+#                   answer_key: "Representational State Transfer", 
+#                   answer_a: "Representative State Transference", 
+#                   answer_b: "Represent States Transference", 
+#                   answer_c: "Representational Status Transfer", 
+#                   answer_d: "Representational State Transfer", 
+#                   true_false: false)
+
+
+# Question.create!(
+#                   description: "Where do validations go?", 
+#                   category: "Rails", 
+#                   difficulty_level: 3, 
+#                   answer_key: "In the Model", 
+#                   answer_a: "In the Model", 
+#                   answer_b: "In the Controller", 
+#                   answer_c: "In the View", 
+#                   answer_d: "In the Database", 
+#                   true_false: false
+#                   )
+
+# Question.create!(
+#                   description: "True or False: Every validations gets a new line.", 
+#                   category: "Rails", 
+#                   difficulty_level: 2, 
+#                   answer_key: "true", 
+#                   answer_a: "", 
+#                   answer_b: "", 
+#                   answer_c: "", 
+#                   answer_d: "", 
+#                   true_false: true) 
+
+
+# Question.create!(
+#                   description: "What do validations do?", 
+#                   category: "Rails", 
+#                   difficulty_level: 2, 
+#                   answer_key: "Validations ensure that only valid data is saved into the database", 
+#                   answer_a: "Validations prove that the database has been creatred", 
+#                   answer_b: "Validations ensure that only valid data is saved into the database", 
+#                   answer_c: "Validations ensure that your Rails app is MIT accepted", 
+#                   answer_d: "Validations ensure that any data can be saved into the database", 
+#                   true_false: false
+#                   )
+
+# Question.create!(
+#                   description: "What is it called when any value entered into a category is distinct from all other values saved in the database under that category?", 
+#                   category: "Rails", 
+#                   difficulty_level: 2, 
+#                   answer_key: "Uniqueness", 
+#                   answer_a: "Original", 
+#                   answer_b: "Uniqueness", 
+#                   answer_c: "Originality", 
+#                   answer_d: "Unique", 
+#                   true_false: false
+#                   )
+
+# Question.create!(
+#                   description: "In validations, when a value has been entered in a given category, this is called what?", 
+#                   category: "Rails", 
+#                   difficulty_level: 2, 
+#                   answer_key: "Presence", 
+#                   answer_a: "Presence", 
+#                   answer_b: "Attendance", 
+#                   answer_c: "Uniqueness", 
+#                   answer_d: "Numericality", 
+#                   true_false: false
+#                   )
+
+# Question.create!(
+#                   description: "True or False: You can add a numericality validation to a string.", 
+#                   category: "Rails", 
+#                   difficulty_level: 2, 
+#                   answer_key: "false", 
+#                   answer_a: "", 
+#                   answer_b: "", 
+#                   answer_c: "", 
+#                   answer_d: "", 
+#                   true_false: true
+#                   )
+
+
+# Question.create!(
+#                   description: "Ruby is an OOP language inspired by what two other languages?", 
+#                   category: "Ruby", 
+#                   difficulty_level: 3, 
+#                   answer_key: "PERL and PYTHON", 
+#                   answer_a: "PERL and PYTHON", 
+#                   answer_b: "PYTHON and JAVA", 
+#                   answer_c: "PYTHON and C++", 
+#                   answer_d: "PERL and JavaScript", 
+#                   true_false: false
+#                   )
+
+# Question.create!(
+#                   description: "What is Rails?", 
+#                   category: "Rails", 
+#                   difficulty_level: 1, 
+#                   answer_key: "It is a framework used for building web applications", 
+#                   answer_a: "It is a system for developing new higher level programming languages", 
+#                   answer_b: "It is a simple library used for building APIs", 
+#                   answer_c: "It is a compiler used for building web applications", 
+#                   answer_d: "It is a framework used for building web applications", 
+#                   true_false: false
+#                   )
+
+# Question.create!(
+#                   description: "What are the three default environments that Rails comes with?", 
+#                   category: "Rails", 
+#                   difficulty_level: 3, 
+#                   answer_key: "testing, development, and production", 
+#                   answer_a: "testing, creation, and production", 
+#                   answer_b: "testing, development, and Transference", 
+#                   answer_c: "testing, development, and production", 
+#                   answer_d: "testing, development, and building", 
+#                   true_false: false
+#                   )
+
+# Question.create!(
+#                   description: "Ruby Global variables begin with what?", 
+#                   category: "Rails", 
+#                   difficulty_level: 3, 
+#                   answer_key: "$", 
+#                   answer_a: "@", 
+#                   answer_b: "@@", 
+#                   answer_c: "$$", 
+#                   answer_d: "$", 
+#                   true_false: false
+#                   )
+
+# Question.create!(
+#                   description: "What do you call it when a value has a numerical value?", 
+#                   category: "Rails", 
+#                   difficulty_level: 1, 
+#                   answer_key: "numericality", 
+#                   answer_a: "numericality", 
+#                   answer_b: "numbering", 
+#                   answer_c: "numerical presence", 
+#                   answer_d: "mathematical", 
+#                   true_false: false
+#                   )
+
+# Question.create!(
+#                   description: "Who created Ruby?", 
+#                   category: "Ruby", 
+#                   difficulty_level: 2, 
+#                   answer_key: "Yukihiro Matsumoto", 
+#                   answer_a: "Yoshihiro Matsumoto", 
+#                   answer_b: "David Heinemeier Hansson", 
+#                   answer_c: "David Hasselhoff", 
+#                   answer_d: "Yukihiro Matsumoto", 
+#                   true_false: false
+#                   )
+
+# Question.create!(
+#                   description: "There were two potential names forRuby. In the end, Ruby was, obviously, the name chosen. What was the other proposed name", 
+#                   category: "Ruby", 
+#                   difficulty_level: 3, 
+#                   answer_key: "Coral", 
+#                   answer_a: "Diamond", 
+#                   answer_b: "Coral", 
+#                   answer_c: "Coffee", 
+#                   answer_d: "Oyster", 
+#                   true_false: false
+#                   )
+
+# Question.create!(
+#                   description: "True or False: Ruby understands and uses 'null' insstead of 'nil'", 
+#                   category: "Ruby", 
+#                   difficulty_level: 3, 
+#                   answer_key: "false", 
+#                   answer_a: "", 
+#                   answer_b: "", 
+#                   answer_c: "", 
+#                   answer_d: "", 
+#                   true_false: true
+#                   )
+
+
+# =========================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
  
